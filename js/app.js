36613702ae64003b0a50f998429733c9ba131bb8
rythm.dipen <rythm.dipen@gmail.com>
2022-01-25 20:21:07 +0545

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// array 
+
+//by using array constructor
+
+// const arr = new Array(1, 2, 3 , 4, 5, 'Deep', true, 4.3);
+
+//  console.log(arr);
+
+//using square brackets[]
+
+// const data=['ram', true, 5.5, 1 ,2 ,3 ,4, null, undefined];
+
+
+
+// console.log(data)
+
+// document.write(data+'<br>'+'number of elements in array='+data.length);
+
+//using split
+
+// let name='Deependra Adhikari';
+// const arrName=name.split('');
+
+// console.log(name);
+
+// console.log(arrName);
+//document.write(arrName);
+
+let fruits='mango, banana, apple, kiwi';
+console.log(fruits);
+const fruitarray = fruits.split(',');
+console.log(fruitarray);
---
+++ js/control_statement.js
@@ -0,0 +1,77 @@
+//for
+//while
+//do-while
+//for-in loop
+
+// for(var i=0; i<5; i++)
+// {
+//     document.write('my name is khan <br>');
+//     document.write('hello khan<br>');
+// }
+document.write('by using while <br>');
+var i = -10;
+
+while(i>=0)
+{
+    document.write(i+'<br>');
+    i--;
+}
+document.write('by using do-while <br>');
+i=-10;
+do{
+    document.write(i+'<br>');
+    i--;
+}while(i>=0);
+
+
+
+//var a=prompt('Enter any number');
+
+//document.write(typeof(a));
+//b=Number(a);
+//document.write(typeof(b));
+
+// if(a>=0)
+// {
+//     document.write('the number is ');
+//     document.write('positive');
+// }
+
+// else
+// {
+//     document.write('the number is negative');
+// }
+
+// switch(b)
+// {
+//     case 1 :
+//     document.write('today is sunday');
+//     break;
+
+//     case 2 :
+//     document.write('today is monday');
+//     break;
+
+//     case '3' :
+//     document.write('today is tuesday');
+//     break;
+//     case '4' :
+//     document.write('today is wednesday');
+//     break;
+
+//     case '5' :
+//     document.write('today is thrusday');
+//     break;
+
+//     case '6' :
+//     document.write('today is fridayday');
+//     break;
+
+//     case '7' :
+//     document.write('today is saturday');
+//     break;
+
+//     default :
+//     document.write('no such a option');
+
+// }
